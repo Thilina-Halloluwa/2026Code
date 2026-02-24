404e49a9f1fe0a88ec7975c89585ccdef66b3d74
hkthi <hkthilina@gmail.com>
2026-02-24 18:46:16 +1000

    Week one code added

--- src/Week01/Marks.java
+++ src/Week01/Marks.java
@@ -2,18 +2,18 @@ package Week01;
 
 public class Marks {
     public static void main(String[] args) {
-        double mark = 78.5;
+        double mark = 63;
         String grade;
 
         if (mark >= 83.00 && mark <= 100.00) {
             grade = "High Distinction";
-        } else if (mark >= 73.00 && mark <= 82.99) {
+        } else if (mark >= 73.00) {
             grade = "Distinction";
-        } else if (mark >= 63.00 && mark <= 72.99) {
+        } else if (mark >= 63.00) {
             grade = "Credit";
-        } else if (mark >= 50.00 && mark <= 62.99) {
+        } else if (mark >= 50.00) {
             grade = "Pass";
-        } else if (mark >= 0.00 && mark <= 49.99) {
+        } else if (mark >= 0.00) {
             grade = "Fail";
         } else {
             grade = "Invalid mark";
