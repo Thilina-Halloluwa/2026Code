b6378671d272ac7ff6ae33048f2368c969eba82e
hkthi <hkthilina@gmail.com>
2026-02-25 10:38:02 +1000

    Non static method added

--- src/Week01/ArithmeticEx.java
+++ src/Week01/ArithmeticEx.java
@@ -3,10 +3,17 @@ package Week01;
 public class ArithmeticEx {
     public static void main(String[] args) {
         add_two_numbers(2, 3);
+//        ArithmeticEx ex = new ArithmeticEx();
+//        ex.add_two_numbers_non(2,3);
     }
 
     static int add_two_numbers(int num1, int num2) {
         int result = num1 + num2;
         return result;
     }
+
+//    int add_two_numbers_non(int num1, int num2) {
+//        int result = num1 + num2;
+//        return result;
+//    }
 }
