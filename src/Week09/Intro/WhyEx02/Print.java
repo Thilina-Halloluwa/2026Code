1f8afffd8f7498d976a72858cb897e9122cd3264
hkthilina <hkthilina@gmail.com>
2026-04-29 17:54:52 +1000

    Added a comment on raw types

--- src/Week09/Intro/WhyEx02/Print.java
+++ src/Week09/Intro/WhyEx02/Print.java
@@ -3,10 +3,11 @@ package Week09.Intro.WhyEx02;
 public class Print<T> {
     T t;
 
-    Print(T t){
+    Print(T t) {
         this.t = t;
     }
-    public void print(){
+
+    public void print() {
         System.out.println(t);
     }
 
@@ -20,7 +21,19 @@ public class Print<T> {
         Print<String> print3 = new Print<>("1.1");
         print3.print();
 
-        Print print4 = new Print<>("1.1");
+       /*
+       The following also work because these are using the raw type Print.
+       A raw type means Java treats it roughly like:  Print<Object>
+       But this loses type safety.
+       For example:
+       Print<String> print = new Print("1.1"); // raw type allowed, but unsafe
+       The compiler may allow it with a warning, but it is not recommended.
+
+       These work because raw types exist for backward compatibility with old Java code before generics.
+       But using them bypasses the type-safety benefits of generics.
+        */
+
+        Print print4 = new Print("1.1");
         print4.print();
         Print print5 = new Print<>(1.1);
         print5.print();
